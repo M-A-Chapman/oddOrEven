4f1339a3f9652092d24e4d168d65d8fb56c13263
Mike <64697794+M-Chapman@users.noreply.github.com>
2023-03-10 14:55:22 +0000

    added odd or even check

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace oddOrEVen;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (var i = 0; i <= 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
